f8a459b9ef8ae7a88000c0f18f32f921a6b0a96e
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-09-13 12:58:26 +0300

    completed

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return `${string.charAt(0).toUpperCase()}${string.slice(1)}`
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g,"");
+  }
+
+  static titleize(string) {
+    let myString = string.charAt(0).toUpperCase() + string.slice(1);
+    const wordArray = myString.split(" ");
+    //array o words that should not be titleize
+    const preposition = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from'];
+
+    //titleize other words except prepoaitions
+    const words = [];
+    for(let word of wordArray) {
+      if (preposition.includes(word)) {
+        words.push(word)
+      } else {
+        words.push(this.capitalize(word));
+      }
+    }
+    return words.join(" ");
+
+  }
 }
